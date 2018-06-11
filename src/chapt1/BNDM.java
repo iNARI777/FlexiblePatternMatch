a498a9a251b068b7eca21348237c19c586db094e
iNARI <pdr93911@126.com>
2018-06-11 17:23:31 +0800

    增加了BNDM算法

--- src/chapt1/BNDM.java
+++ src/chapt1/BNDM.java
@@ -1,6 +1,10 @@
 package algorithms;
 
 public class BNDM {
+    /*
+    为每个字符生成一个bitmap用来标识其在pattern中出现的位置，因为是放在
+    long类型的变量中，所以要求pattern的长度不能大于64
+    */
     private long[] setBitmaps(String pattern) {
         long[] bitmaps = new long[256];
 
@@ -16,23 +20,16 @@ public class BNDM {
         long target = 1 << pattern.length() - 1;
 
         for (int i = 0; i <= content.length() - pattern.length(); ) {
-            long status = -1, last = pattern.length();
+            long status = -1, step = pattern.length();  // status是当前的匹配状态（初始为全1），step是下次窗口移动的距离（初始为pattern长度）
             for(int j = pattern.length() - 1; j >= 0; j--) {
-                status = status & bitmaps[content.charAt(i + j)];
+                status = status & bitmaps[content.charAt(i + j)];   // 可以得到当前窗口中的content是否有pattern中存在的子字符串
                 if(status == 0) break;
-                if((status & target) != 0) last = j;
-                status <<= 1;
+                if((status & target) != 0) step = j;    // 当前遍历到的状态在pattern的开头（即当前content的后缀为pattern的一个前缀），要减小step
+                status <<= 1;                           // 准备下一次状态改变
             }
             if(status != 0) return true;
-            i += last;
+            i += step;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        String pattern = "abcab";
-//        String content = "bcabcab";
-        String content = "abcaabcbcaabcab";
-        System.out.println(new BNDM().hasPattern(content, pattern));
     }
 }
